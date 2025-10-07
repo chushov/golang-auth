12bdcae4af82cbacc618698e53a4f2a6455b986c
Dmitry Chushov <dchushov@gmail.com>
2025-10-07 13:40:44 +0300

    refactor: переход на переменные окружения для БД и порта
    - Добавлен импорт "os" и "fmt"
    - Строка подключения к БД формируется из переменных окружения: DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
    - Порт приложения читается из переменной APP_PORT с дефолтным значением 8080
    - Убраны хардкоденные значения для гибкой настройки через .env

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"golang-auth/controllers"
 	"golang-auth/database"
 	"golang-auth/service"
+	"os"
 
 	"github.com/gin-gonic/gin" // TODO: Найти аналог GIN-фреймворку
 )
 
 func main() {
-	database.Connect("golang-auth:lD1hSN2fs6mVh1Sj1uC7j_9J@tcp(localhost:3306)/jwt_golang_auth?parseTime=true")
+	// Формируем строку подключения из переменных окружения
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+	database.Connect(dsn)
 	database.Migrate()
 
 	router := initRouter()
-	err := router.Run(":8080")
+
+	// Читаем порт из переменной окружения с дефолтным значением 8080
+	appPort := os.Getenv("APP_PORT")
+	if appPort == "" {
+		appPort = "8080"
+	}
+
+	err := router.Run(":" + appPort)
 	if err != nil {
 		return
 	}
@@ -21,7 +38,6 @@ func main() {
 
 func initRouter() *gin.Engine {
 	router := gin.Default()
-
 	api := router.Group("/api")
 	{
 		api.POST("/token", controllers.GenerateToken)
